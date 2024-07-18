f3f7385285dfa6299578dd472e5497fee9994cc0
madhu dharani <cmdharani2014@gmail.com>
2024-07-18 22:40:49 +0530

    Add project files.

---
+++ CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController:ControllerBase
+    {
+        [HttpGet]
+        public string GetStudentName()
+        {
+            return "My Name is Dharani";
+        }
+    }
+}
