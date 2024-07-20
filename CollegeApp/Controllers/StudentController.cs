3e0cb3690d7d8f1d466bda2f81e4ae5c85b5dc84
madhu dharani <cmdharani2014@gmail.com>
2024-07-20 19:25:53 +0530

    In Memory repository

--- CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CollegeApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CollegeApp.Controllers
 {
@@ -8,9 +10,47 @@ namespace CollegeApp.Controllers
     public class StudentController:ControllerBase
     {
         [HttpGet]
-        public string GetStudentName()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IEnumerable<Student> GetAllStudents()
         {
-            return "My Name is Dharani";
+            return CollegeRepository.Students;
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<Student> GetStudentsById(int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+
+            var result = CollegeRepository.Students.FirstOrDefault(x => x.Id == id);
+
+            if(result == null)
+                return NotFound($"student with the id {id} not found");
+
+            return result;
+        }
+
+        [HttpGet("{name:alpha}",Name= "GetStudentByName")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<Student> GetStudentByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return BadRequest();
+
+            var result = CollegeRepository.Students.FirstOrDefault(x => x.StudentName == name);
+
+            if (result == null)
+                return NotFound($"student with the id {name} not found");
+
+            return result;
         }
     }
 }
---
+++ CollegeApp/Models/CollegeRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace CollegeApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>()
+        {
+            new() { Id=1, Address="Bangalore", Email="test@123.com",StudentName="dharani" },
+            new (){ Id=2, Address="Delhi", Email="testABC@123.com",StudentName="Madhu" }
+
+
+
+        };
+    }
+}
