bf36f7a5498b5a1c073d8df2009e9f76fe540bbb
madhu dharani <cmdharani2014@gmail.com>
2024-07-21 13:34:24 +0530

    TestCommit

--- CollegeApp/DTO/StudentDto.cs
+++ CollegeApp/DTO/StudentDto.cs
@@ -6,5 +6,6 @@
         public string StudentName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
+
     }
 }
